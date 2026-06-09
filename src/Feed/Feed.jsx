924c29db7df14fb4300d7ccbbc76628f66b45579
Shirley Butti <shirleybutti12@gmail.com>
2026-06-09 11:28:50 +0400

    path changes

--- src/Feed/Feed.jsx
+++ src/Feed/Feed.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "../Utils/constants";
+import { BASE_URL } from "../Utils/Constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../Utils/Redux/feedSlice";
 import { useEffect } from "react";
